2e592084f3d7baeedb3d59b368c6be41265f1e2f
Victor VickyJay <victoluolatunji@gmail.com>
2024-06-05 06:48:31 +0100

    setup complete

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const META_TITLE: string = ">Studioperks";
+export const META_DESCRIPTION: string =
+  "we are committed to capturing your vision and bringing it to life through exceptional visual storytelling";
+export const META_IMAGE: string = "";
+
+export const META_URL: string = "https://studioperks.vercel.app/"; //will change to  the real domain later
+export const META_TYPE: string = "website";
+export const META_AUTHOR: string = "Studioperks Dev Team";
+export const META_PUBLISHER: string = "Studioperks Inc";
+export const META_KEYWORDS: string =
+  "Studioperks, Camera, Photography, Videos, Audio, Creativity, design";
+
+// api url
+export const BASE_API_URL = "";
---
+++ src/pages/not-found.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+// import { SEO } from "../components/common";
+
+export default function NotFound() {
+  return (
+    <>
+      {/* <SEO title="Page not found, pay $404 to access this page" /> */}
+      <div className="text-black text-center font-semibold text-xl">
+        You are not supposed to see this page due to legal reasons.
+        <br /> go to&nbsp;
+        <span className="text-vista underline">
+          <Link to="/">home</Link>
+        </span>
+      </div>
+    </>
+  );
+}
