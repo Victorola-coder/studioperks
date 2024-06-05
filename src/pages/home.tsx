7f47c8185f46c16d2a1cb8e0ec744346adb78f79
Victor VickyJay <victoluolatunji@gmail.com>
2024-06-05 17:05:20 +0100

    init hero section/home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,12 @@
 import { Fragment } from "react";
+import { Hero } from "../components/home";
 
 export default function Home() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <main>
+        <Hero />
+      </main>
+    </Fragment>
+  );
 }
