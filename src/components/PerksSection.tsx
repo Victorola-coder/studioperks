9ec5daed146e9aa18b68a95c90cb5a646dc95529
VickyJay <victoluolatunji@gmail.com>
2024-07-04 21:25:33 +0100

    minor updates

--- src/components/PerksSection.tsx
+++ src/components/PerksSection.tsx
@@ -1,12 +1,12 @@
 const PerksSection = () => {
   return (
-    <div className=" h-[73.96px] md:h-[196px] bg-black relative flex ">
+    <div className="h-[73.96px] md:h-[196px] bg-black relative flex ">
       <img
-        className=" absolute -left-[14px] md:left-[33px] top-1/2 -translate-y-1/2 md:w-[143px] h-[56.98px] w-[53.96px] md:h-[151px] object-cover"
+        className="absolute -left-[14px] md:left-[33px] top-1/2 -translate-y-1/2 md:w-[143px] h-[56.98px] w-[53.96px] md:h-[151px] object-cover"
         src="/images/logotrans.svg"
         alt="logo"
       />
-      <div className=" h-full w-full flex items-center justify-center left-[25px] md:-translate-x-1/2 md:left-1/2 top-0 ">
+      <div className="h-full w-full flex items-center justify-center left-[25px] md:-translate-x-1/2 md:left-1/2 top-0 ">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((logo) => (
           <img
             key={logo}
