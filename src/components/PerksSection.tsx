61681e2fabca1fb2a41f04e0f3aeb581d718e9a6
kuipid01 <kuipid01@gmnail.com>
2024-07-17 13:17:55 +0100

    added animation

--- src/components/PerksSection.tsx
+++ src/components/PerksSection.tsx
@@ -1,57 +1,49 @@
-import { motion } from "framer-motion";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 const PerksSection = () => {
+  const img1 = useRef<HTMLImageElement | null>(null);
+  const img2 = useRef<HTMLImageElement | null>(null);
+  const perkRef = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(() => {
+    if (img1.current && img2.current) {
+      const animateImage = (element: HTMLImageElement, delay: number = 0) => {
+        const timeline = gsap.timeline({ repeat: -1, delay });
+        timeline
+          .to(element, { left: "90%", duration: 2, ease: "power1.inOut" })
+          .to(element, { left: "-2%", duration: 1, ease: "power3.out" });
+      };
+
+      animateImage(img1.current);
+      animateImage(img2.current, 0.2);
+    }
+    if (perkRef) {
+      gsap.to(perkRef.current, { x: "-2005", duration: 6, repeat: -1 });
+    }
+  });
+
   return (
     <div className="h-[73.96px] w-full md:h-[196px] bg-black relative flex">
-      {/* <motion.img
-        initial={{
-          left: "0%",
-          top: "50%",
-          translateY: "-50%",
-          rotate: 0,
-        }}
-        animate={{
-          left: ["0%", "50%", "90%"],
-          rotate: [0, 180, 360],
-        }}
-        transition={{
-          duration: 4,
-          ease: "linear",
-          repeat: Infinity,
-          repeatType: "reverse",
-
-          repeatDelay: 2,
-        }}
-        className="absolute -left-[14px] b md:w-[143px] h-[56.98px] w-[53.96px] md:h-[151px] object-cover"
-        src="/images/logotrans.svg"
-        alt="logo"
-      /> */}
-      <motion.img
-        initial={{
-          left: "0%",
-          top: "50%",
-          translateY: "-50%",
-          rotate: 0,
-        }}
-        animate={{
-          left: ["0%", "50%", "90%"],
-
-          rotate: [0, 180, 360],
-        }}
-        transition={{
-          duration: 4,
-          ease: "easeInOut",
-          times: [0, 0, 1],
-          repeat: Infinity,
-          repeatType: "reverse",
-          delay: 0.1,
-          repeatDelay: 2,
-        }}
-        className="absolute -left-[14px] md:w-[143px] h-[56.98px] w-[53.96px] md:h-[151px] object-cover"
+      <img
+        ref={img1}
+        className="absolute top-1/2 -translate-y-1/2 -left-[2%] md:w-[143px] h-[56.98px] w-[53.96px] md:h-[151px] object-cover"
         src="/images/logotrans.svg"
         alt="logo"
       />
-      <div className="h-full w-full flex items-center justify-center left-[25px] md:-translate-x-1/2 md:left-1/2 top-0">
+      <img
+        ref={img2}
+        className="absolute top-1/2 -translate-y-1/2 -left-[2%] md:w-[143px] h-[56.98px] w-[53.96px] md:h-[151px] object-cover"
+        src="/images/logotrans.svg"
+        alt="logo"
+      />
+      <div
+        ref={perkRef}
+        className="h-full w-full flex items-center justify-center left-[25px] md:-translate-x-1/2 md:left-1/2 top-0"
+      >
         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((logo) => (
           <img
             key={logo}
